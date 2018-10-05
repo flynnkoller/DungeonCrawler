e6ac3043c74365d24e69aae3d51152f6fb329bb1
FrogFace <tobyr431@hotmail.co.uk>
2018-10-05 07:25:28 +0100

    Updated collisions
    Reduced collision scale to ocunter walking through wall joints

--- Assets/WallDetector.cs
+++ Assets/WallDetector.cs
@@ -5,24 +5,15 @@ using UnityEngine;
 public class WallDetector : MonoBehaviour {
 
     public bool contact;
-
-    void Start()
-    {
-        gameObject.GetComponent<Renderer>().material.color = Color.green;
-    }
-
-
+    
     public void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Contact");
         contact = true;
-        gameObject.GetComponent<Renderer>().material.color = Color.red;
     }
 
      void OnCollisionExit(Collision collision)
     {
         contact = false;
-        gameObject.GetComponent<Renderer>().material.color = Color.green;
     }
 
 }

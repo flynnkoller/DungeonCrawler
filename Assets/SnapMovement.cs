5eb83ff665a4d39ce6eb074e539dafabedff1038
FrogFace <tobyr431@hotmail.co.uk>
2018-10-05 07:09:02 +0100

    Initial commit

---
+++ Assets/SnapMovement.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapMovement : MonoBehaviour
+{
+    Vector3 startPos = new Vector3();
+    Vector3 endPos = new Vector3();
+    float fraction = 0;
+    bool moving = false;
+
+    public GameObject rightCol;
+    public GameObject rearCol;
+    public GameObject leftCol;
+    public GameObject frontCol;
+
+    void Start()
+    {
+
+
+    }
+
+    void Update()
+    {
+        
+        
+
+        if (Input.GetKey("d") && !moving)
+        {
+            if (!rightCol.GetComponent<WallDetector>().contact)
+            {
+                startPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+                endPos = new Vector3(transform.position.x + 1f, transform.position.y, transform.position.z);
+                fraction = 0;
+                moving = true;
+            }
+        }
+
+        if (Input.GetKey("a") && !moving)
+        {
+            if (!leftCol.GetComponent<WallDetector>().contact)
+            {
+                startPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+                endPos = new Vector3(transform.position.x - 1f, transform.position.y, transform.position.z);
+                fraction = 0;
+                moving = true;
+            }
+        }
+
+        if (Input.GetKey("w") && !moving)
+        {
+            if (!frontCol.GetComponent<WallDetector>().contact)
+            {
+                startPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+                endPos = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1f);
+                fraction = 0;
+                moving = true;
+            }
+        }
+
+        if (Input.GetKey("s") && !moving)
+        {
+            if (!rearCol.GetComponent<WallDetector>().contact)
+            {
+                startPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+                endPos = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1f);
+                fraction = 0;
+                moving = true;
+            }
+        }
+
+
+        if (moving)
+        {
+            if (fraction < 1)
+            {
+                fraction += Time.deltaTime * 7f;
+                transform.position = Vector3.Lerp(startPos, endPos, fraction);
+            }
+        }
+
+        if (transform.position == endPos && moving)
+        {
+            moving = false;
+        }
+        
+    }
+
+    
+
+  
+    
+}
---
+++ Assets/WallDetector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDetector : MonoBehaviour {
+
+    public bool contact;
+
+    void Start()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.green;
+    }
+
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Contact");
+        contact = true;
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+     void OnCollisionExit(Collision collision)
+    {
+        contact = false;
+        gameObject.GetComponent<Renderer>().material.color = Color.green;
+    }
+
+}
